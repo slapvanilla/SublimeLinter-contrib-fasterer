b8edff7ba14d080607c7eae24627e23f58a90309
slapvanilla <slapvanilla@gmail.com>
2018-05-13 11:23:45 -0500

    Replace deprecated features with current recommendations

--- linter.py
+++ linter.py
@@ -1,26 +1,16 @@
-from SublimeLinter.lint import RubyLinter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import RubyLinter
 
 class Fasterer(RubyLinter):
   """Provides an interface to fasterer."""
-
-  syntax = (
-      'better rspec',
-      'betterruby',
-      'cucumber steps',
-      'rspec',
-      'ruby experimental',
-      'ruby on rails',
-      'ruby'
-  )
   cmd = 'ruby -S fasterer'
-  # Using each_with_index is slower than while loop. Occurred at lines: 1469.
+
+  # Message Ex: Using each_with_index is slower than while loop. Occurred at lines: 1469.
   regex = r'^(?P<message>.+). Occurred at lines: (?P<line>\d+)'
+
+  defaults = {
+    'selector': 'source.ruby'
+  }
+
+  default_type = SublimeLinter.lint.WARNING
   tempfile_suffix = 'rb'
-
-  # Versioning
-  version_args = '-S gem list | grep fasterer'
-  version_re = r'fasterer \((?P<version>\d+\.\d+\.\d+)\)'
-  version_requirement = '>= 0.4.1'
-
-  # Other Options
   multiline = False
